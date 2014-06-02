d4dd2a54777aadaae1b1d0150e58e621c8a3d89b
Yasuhiro Usutani <usutani@tgl.co.jp>
2014-06-02 09:41:22 +0900

    Callbackを回避する

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   enum status: [:registered, :associated, :retired, :deleted]
+
+  after_update do
+    Net::HTTP.get(URI.parse('http://www.example.com'))
+    sleep 10
+  end
 end
--- db/migrate/20140602003707_change_users_status_type.rb
+++ db/migrate/20140602003707_change_users_status_type.rb
@@ -3,13 +3,18 @@ class ChangeUsersStatusType < ActiveRecord::Migration
     rename_column :users, :status, :status_string
     add_column :users, :status, :integer
     User.find_each do |user|
-      user.update(status: user.status_string.try(:downcase))
+      raw_status = if user.status_string
+                     User.statuses[user.status_string.downcase]
+                   else
+                     nil
+                   end
+      user.update_column(:status, raw_status)
     end
   end
 
   def down
-    User.find_each do | user |
-      user.update(status_string: user.status.try(:capitalize))
+    User.find_each do |user|
+      user.update_column(:status_string, user.status.try(:capitalize))
     end
     remove_column :users, :status
     rename_column :users, :status_string, :status
